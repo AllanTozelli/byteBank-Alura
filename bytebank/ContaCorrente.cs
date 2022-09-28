31cabaa2197f43d491ead1628bbb20670a07e431
Allan Toelli <allan_tozelli@hotmail.com>
2022-09-28 18:08:40 -0300

    ByteBank Project

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,140 @@
+﻿using bytebank.Titular;
+using System.Globalization;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }     
+        
+        public string Conta
+        {
+            get { return _conta; }
+            set { if (value == null) { return; } else { _conta = value; } }
+        }        
+        public int NumAgencia { 
+        get { return _numAgencia; }
+            set { if(value<= 0) { } _numAgencia = value; }
+        }
+        public string NomeAgencia { get; set; }
+        private double Saldo;
+
+
+
+        private string _conta;
+        private int _numAgencia;
+
+
+
+        public ContaCorrente(int numAgencia, string conta)
+        {
+            NumAgencia = numAgencia;
+            Conta = conta;
+            TotalDeContasCriadas++;
+        }
+
+        public ContaCorrente(Cliente titular, string conta, int numAgencia, string nomeAgencia, double saldo)
+        {
+            Titular = titular;
+            Conta = conta;        
+            NumAgencia = numAgencia;           
+            NomeAgencia = nomeAgencia;
+            Saldo = saldo;
+            TotalDeContasCriadas++;
+            
+        }
+
+        public static int TotalDeContasCriadas { get; set; }
+        public double saldo
+        {
+            get
+            {
+                return Saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    Saldo += value;
+                }
+            }
+        }       
+
+        public bool Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                return false;
+            }
+            else if (Saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                Saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+
+            }
+            else
+            {
+                saldo += valor;
+
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else if (valor < 0)
+            {
+                return false;
+
+            }
+            else
+            {
+                saldo -= valor;
+                destino.saldo += valor;
+                return true;
+            }
+        }
+
+        public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular :" + Titular);
+            Console.WriteLine("Conta :" + Conta);
+            Console.WriteLine("Número Agência :" + NumAgencia);
+            Console.WriteLine("Nome Agência :" + NomeAgencia);
+            Console.WriteLine("Saldo: " + saldo);
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Conta
+                + ", Titular:"
+                + Titular.Nome
+                + ", CPF:"
+                + Titular.Cpf
+                + ", Profissão:"
+                + Titular.Profissao
+                + ", Saldo: R$"
+                + saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using bytebank;
+using bytebank.Titular;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Boas Vindas ao seu banco, byteBank!");
+
+        Cliente sarah = new Cliente();
+        sarah.Nome = "Sarah Silva";
+        sarah.Profissao = "Professora";
+        sarah.Cpf = "11111111-12";
+
+        Cliente ester = new Cliente();
+        ester.Nome = "Ester Almeida";
+        ester.Profissao = "Advogada";
+        ester.Cpf = "868524125-32";        
+
+        ContaCorrente contaAndre = new ContaCorrente(159, "152869-x");
+        contaAndre.Titular = new Cliente();
+        contaAndre.Titular.Nome = " André Pereira";
+        contaAndre.Titular.Profissao = "Auxiliar Administrativo";
+        contaAndre.saldo = 100;
+        Console.WriteLine();
+        Console.WriteLine("Dados dos Clientes Cadastrados");
+        Console.WriteLine();
+        Console.WriteLine(sarah.ToString());
+        Console.WriteLine(ester.ToString());
+        Console.WriteLine("Nome:" + contaAndre.Titular.Nome + ", CPF:" + contaAndre.Titular.Cpf
+            + ", Profissão:" + contaAndre.Titular.Profissao);     
+        Console.WriteLine("Total de clientes: " + Cliente.TotalClientesCadastrados);
+        Console.WriteLine();
+        Console.WriteLine("Dados das Conta");
+        Console.WriteLine(contaAndre.ToString());
+        Console.WriteLine("Total de Contas: " + ContaCorrente.TotalDeContasCriadas);
+        Console.WriteLine();
+        
+
+
+        Console.ReadKey();
+
+    }
+}
+
